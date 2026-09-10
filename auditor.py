cc9e66441942ff9213af005d08732ea8897c0048
Isaacs-1123 <2601899@sit.singaporetech.edu.sg>
2026-09-11 01:08:18 +0800

    Requirement 3-6

--- auditor.py
+++ auditor.py
@@ -1,1 +1,13 @@
 inventory = 0
+input_quantity = 0
+
+while input_quantity != "quit":
+        input_quantity = input("Please enter a stock quantity: ")
+        if input_quantity == "quit":
+            input_quantity = "quit"
+            
+        elif input_quantity.isdigit():
+            inventory += int(input_quantity)
+            
+        else:
+            print("Error, this is not a valid input")
