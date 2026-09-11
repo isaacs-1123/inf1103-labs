efbe3fdc21d895d2812de8f012ae1487bb2ad8e0
Isaacs-1123 <2601899@sit.singaporetech.edu.sg>
2026-09-11 08:40:30 +0800

    Trigger Overstock Alert

--- auditor.py
+++ auditor.py
@@ -2,6 +2,10 @@ inventory = 0
 input_quantity = 0
 
 while input_quantity != "quit":
+    if inventory >500:
+        print("Alert! Total inventory has exceeded 500 units.")
+        break 
+    else:    
         input_quantity = input("Please enter a stock quantity: ")
         if input_quantity == "quit":
             input_quantity = "quit"
