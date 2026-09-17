8dc19b48a486738e12a6bb3baea72aa9c7853b67
Isaacs-1123 <2601899@sit.singaporetech.edu.sg>
2026-09-17 23:24:54 +0800

    Get valid input function added

---
+++ modular_auditor.py
@@ -0,0 +1,40 @@
+inventory = 0
+input_quantity = 0
+failed_input = 0
+
+def get_valid_input():
+    user_input = input("Please enter a stock quantity or type quit to exit: ")
+    if user_input == "quit":
+        return "quit"
+    elif user_input.isdigit():
+        return int(user_input)
+    else:
+        print("Error, this is not a valid input.")
+        return None
+
+while input_quantity != "quit":
+    result = get_valid_input()
+
+    if result == "quit":  
+        print("The Total Units Processed is", inventory, "and the Number of Failed/Reject Entries is",failed_input) 
+        input_quantity = "quit"
+
+    elif result is None:
+        failed_input += 1
+
+    else:
+        input_quantity = result
+        inventory = inventory + result
+        if inventory >500:
+            print("Alert! Total inventory has exceeded 500 units.")
+            break 
+
+        else:
+            print("Added", input_quantity, "items to inventory. Total Inventory:",inventory)
+
+
+
+
+    
+
+
